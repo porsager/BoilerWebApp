0b0c143913acdfd0d23148fa7d199d3db44d5788
Rasmus Porsager <rasmus@porsager.com>
2019-04-05 17:55:12 +0200

    Switch to esm dev

--- scripts/dev.js
+++ scripts/dev.js
@@ -1,43 +1,16 @@
 const wright = require('wright')
-    , rollup = require('rollup')
     , sass = require('sass')
-    , commonJs = require('rollup-plugin-commonjs')
-    , nodeResolve = require('rollup-plugin-node-resolve')
 
 wright({
   main: 'assets/index.html',
+  serve: 'src',
   run: true,
-  js: {
-    path: 'index.js',
-    watch: 'src/**/*.js',
-    compile: compileJs
-  },
   css: {
     path: 'style.css',
     watch: 'src/css/**/*.scss',
     compile: compileSass
   }
 })
-
-let cache
-function compileJs() {
-  return rollup.rollup({
-    input: 'src/index.js',
-    cache,
-    plugins: [
-      nodeResolve(),
-      commonJs()
-    ]
-  })
-  .then(bundle => {
-    cache = bundle.cache
-    return bundle.generate({
-      format: 'iife',
-      sourcemap: true
-    })
-  })
-  .then(r => r.output[0])
-}
 
 function compileSass() {
   return new Promise((resolve, reject) => {
